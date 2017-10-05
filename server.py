ed1c0b5f5137eb1d2e5da4f665b92d4a671c81dc
Rodrigo de Carvalho Maia <rodrigo.maia@infoglobo.com.br>
2017-10-04 21:45:06 -0300

    Adicionando esqueleto de alguns métodos e testes

--- server.py
+++ server.py
@@ -3,20 +3,15 @@ import socket
 import requests
 from flask import Flask, request
 
-ip = socket.gethostbyname(socket.gethostname())
 app = Flask(__name__)
-
-url = "http://172.38.0.{}:5000?"
-
+ip = socket.gethostbyname(socket.gethostname())
 relogio = {}
-
-# querystring = "teste=seraquevai"
-# resposta = requests.post("http://172.18.0.3:5000?{}".format(querystring))
-# msg += resposta.text
+url = "http://172.38.0.{}:5000?"
 
 
 @app.route('/', methods=['GET'])
 def ler_valores():
+    # TODO trocar pela implementacao real
     msg = "#########\n"
     msg += "IP: {}\n".format(ip)
     return msg
@@ -24,6 +19,7 @@ def ler_valores():
 
 @app.route('/', methods=['POST'])
 def escrever_valores():
+    # TODO trocar pela implementacao real
     var = request.args.get('teste')
     msg = "{}\n".format(var)
     for arg in request.args:
@@ -51,43 +47,26 @@ def atualizar_relogio(query_string):
     return relogio
 
 
-def envia(id_no, acao):
+def envia_acao(id_no, acao):
     # TODO melhorar mensagem de retorno
     qs = obter_relogio()
     try:
-        retorno = requests.get(url.format(id_no) + "msg=" + acao + qs, timeout=5)
+        retorno = requests.get(url.format(id_no) + "acao=" + acao + qs, timeout=1)
         msg = "this is ok: {}".format(retorno)
     except requests.RequestException as e:
         msg = "not ok: {}".format(e)
     return msg
 
 
+# TODO def incrementa_relogio_interno():
+def incrementa_relogio_interno():
+    pass
+
+
+# TODO def recebe_acao():
+def recebe_acao():
+    pass
+
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
-
-#
-# import socket
-# import os
-# import sys
-# HOST = ''              # Endereco IP do Servidor
-# PORT = 5000            # Porta que o Servidor esta
-# tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# orig = (HOST, PORT)
-# tcp.bind(orig)
-# tcp.listen(1)
-# while True:
-#     con, cliente = tcp.accept()
-#     pid = os.fork()
-#     if pid == 0:
-#         tcp.close()
-#         print('Conectado por', cliente)
-#         while True:
-#             msg = con.recv(1024)
-#             if not msg:
-#                 break
-#             print(cliente, msg)
-#         print('Finalizando conexao do cliente', cliente)
-#         con.close()
-#         sys.exit(0)
-#     else:
-#         con.close()
--- teste_server.py
+++ teste_server.py
@@ -7,7 +7,7 @@ import server
 
 class TesteServer(unittest.TestCase):
     def test_atualizar_relogio(self):
-        qs = "msg=teste_msg&id2=3&id3=4&id4=5&id5=6"
+        qs = "acao=teste_acao&id2=3&id3=4&id4=5&id5=6"
         retorno = server.atualizar_relogio(qs)
         tipo_esperado = dict
         valor_esperado = {"id3": "4", "id4": "5", "id5": "6", "id2": "3"}
@@ -15,7 +15,7 @@ class TesteServer(unittest.TestCase):
         self.assertEqual(retorno, valor_esperado)
 
     def teste_obter_relogio(self):
-        qs = "msg=teste_msg&id2=3&id3=4&id4=5&id5=6"
+        qs = "acao=teste_msg&id2=3&id3=4&id4=5&id5=6"
         server.atualizar_relogio(qs)
         relogio_esperado = {"id2": "3", "id3": "4", "id4": "5", "id5": "6"}
         valor_esperado = "&id2=3&id3=4&id4=5&id5=6"
@@ -30,12 +30,13 @@ class TesteServer(unittest.TestCase):
     @patch("requests.get")
     def teste_envia_com_sucesso(self, mock_get):
         mock_get.return_value = "teste_retorno"
+        server.relogio = {}
         id_no = "1"
         acao = "w3"
-        parametro_esperado = "http://172.38.0.1:5000?msg=w3&id2=0&id3=0&id4=0&id5=0"
+        parametro_esperado = "http://172.38.0.1:5000?acao=w3&id2=0&id3=0&id4=0&id5=0"
         retorno_esperado = "this is ok: teste_retorno"
-        retorno = server.envia(id_no, acao)
-        mock_get.assert_called_with(parametro_esperado, timeout=5)
+        retorno = server.envia_acao(id_no, acao)
+        mock_get.assert_called_with(parametro_esperado, timeout=1)
         self.assertEqual(retorno_esperado, retorno)
 
     def teste_envia_com_falha(self):
@@ -43,8 +44,12 @@ class TesteServer(unittest.TestCase):
         id_no = "1"
         acao = "w3"
         retorno_esperado = "not ok:"
-        retorno = server.envia(id_no, acao)
+        retorno = server.envia_acao(id_no, acao)
         self.assertIn(retorno_esperado, retorno)
+
+    def teste_recebe_acao(self):
+        # TODO Fazer o teste
+        pass
 
 
 if __name__ == '__main__':
