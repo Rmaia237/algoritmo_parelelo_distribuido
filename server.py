c9aeb260adc02d958f34111e0ab8f117dd7a6bcb
Rodrigo de Carvalho Maia <rodrigo.maia@infoglobo.com.br>
2017-10-11 17:09:53 -0300

    Mudando estrutura para usar classe, deixando o arquivo inicial apenas como "front"

--- server.py
+++ server.py
@@ -1,72 +1,42 @@
 import socket
 
 import requests
-from flask import Flask, request
-
-app = Flask(__name__)
-ip = socket.gethostbyname(socket.gethostname())
-relogio = {}
-url = "http://172.38.0.{}:5000?"
 
 
-@app.route('/', methods=['GET'])
-def ler_valores():
-    # TODO trocar pela implementacao real
-    msg = "#########\n"
-    msg += "IP: {}\n".format(ip)
-    return msg
+class Server(object):
+    def __init__(self):
+        self.ip = socket.gethostbyname(socket.gethostname())
+        self.relogio = {}
+        self.url = "http://172.38.0.{}:5000?"
 
+    def obter_relogio(self):
+        qs = ""
+        if not self.relogio:
+            self.relogio = {"id2": "0", "id3": "0", "id4": "0", "id5": "0"}
+        for chave, valor in sorted(self.relogio.items()):
+            qs += "&{}={}".format(chave, valor)
+        return qs
 
-@app.route('/', methods=['POST'])
-def escrever_valores():
-    # TODO trocar pela implementacao real
-    var = request.args.get('teste')
-    msg = "{}\n".format(var)
-    for arg in request.args:
-        msg += "arg -> {}\n".format(arg)
-    return msg
+    def atualizar_relogio(self, query_string):
+        variaveis = str(query_string).split("&")
+        for variavel in variaveis:
+            if variavel.startswith("id"):
+                self.relogio[variavel.split("=")[0]] = variavel.split("=")[1]
 
+    def envia_acao(self, id_no, acao):
+        # TODO melhorar mensagem de retorno
+        qs = self.obter_relogio()
+        try:
+            retorno = requests.get(self.url.format(id_no) + "acao=" + acao + qs, timeout=1)
+            msg = "this is ok: {}".format(retorno)
+        except requests.RequestException as e:
+            msg = "not ok: {}".format(e)
+        return msg
 
-def obter_relogio():
-    global relogio
-    qs = ""
-    if not relogio:
-        relogio = {"id2": "0", "id3": "0", "id4": "0", "id5": "0"}
-    for chave, valor in sorted(relogio.items()):
-        qs += "&{}={}".format(chave, valor)
-    return qs
+    # TODO def incrementa_relogio_interno():
+    def incrementa_relogio_interno(self):
+        pass
 
-
-def atualizar_relogio(query_string):
-    global relogio
-
-    variaveis = str(query_string).split("&")
-    for variavel in variaveis:
-        if variavel.startswith("id"):
-            relogio[variavel.split("=")[0]] = variavel.split("=")[1]
-    return relogio
-
-
-def envia_acao(id_no, acao):
-    # TODO melhorar mensagem de retorno
-    qs = obter_relogio()
-    try:
-        retorno = requests.get(url.format(id_no) + "acao=" + acao + qs, timeout=1)
-        msg = "this is ok: {}".format(retorno)
-    except requests.RequestException as e:
-        msg = "not ok: {}".format(e)
-    return msg
-
-
-# TODO def incrementa_relogio_interno():
-def incrementa_relogio_interno():
-    pass
-
-
-# TODO def recebe_acao():
-def recebe_acao():
-    pass
-
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    # TODO def recebe_acao():
+    def recebe_acao(self):
+        pass
---
+++ start.py
@@ -0,0 +1,28 @@
+from flask import Flask, request
+
+from server import Server
+
+app = Flask(__name__)
+server = Server()
+
+
+@app.route('/', methods=['GET'])
+def ler_valores():
+    # TODO trocar pela implementacao real
+    msg = "#########\n"
+    msg += "IP: {}\n".format(server.ip)
+    return msg
+
+
+@app.route('/', methods=['POST'])
+def escrever_valores():
+    # TODO trocar pela implementacao real
+    var = request.args.get('teste')
+    msg = "{}\n".format(var)
+    for arg in request.args:
+        msg += "arg -> {}\n".format(arg)
+    return msg
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
