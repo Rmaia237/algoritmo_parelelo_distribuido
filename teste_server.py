7713e007c0905eba13bbbd51a6e35abb91d02acd
Rodrigo de Carvalho Maia <rodrigo.maia@infoglobo.com.br>
2017-10-11 18:58:58 -0300

    Ajustando classe de teste

--- teste_server.py
+++ teste_server.py
@@ -2,13 +2,16 @@ import unittest
 
 from mock import patch
 
-import server
+from server import Server
 
 
 class TesteServer(unittest.TestCase):
+    def setUp(self):
+        self.server = Server()
+
     def test_atualizar_relogio(self):
         qs = "acao=teste_acao&id2=3&id3=4&id4=5&id5=6"
-        retorno = server.atualizar_relogio(qs)
+        retorno = self.server.atualizar_relogio(qs)
         tipo_esperado = dict
         valor_esperado = {"id3": "4", "id4": "5", "id5": "6", "id2": "3"}
         self.assertEqual(type(retorno), tipo_esperado)
@@ -16,26 +19,26 @@ class TesteServer(unittest.TestCase):
 
     def teste_obter_relogio(self):
         qs = "acao=teste_msg&id2=3&id3=4&id4=5&id5=6"
-        server.atualizar_relogio(qs)
+        self.server.atualizar_relogio(qs)
         relogio_esperado = {"id2": "3", "id3": "4", "id4": "5", "id5": "6"}
         valor_esperado = "&id2=3&id3=4&id4=5&id5=6"
-        self.assertEqual(server.relogio, relogio_esperado)
-        self.assertEqual(server.obter_relogio(), valor_esperado)
+        self.assertEqual(self.server.relogio, relogio_esperado)
+        self.assertEqual(self.server.obter_relogio(), valor_esperado)
 
     def teste_obter_relogio_vazio(self):
-        server.relogio = {}
+        self.server.relogio = {}
         valor_esperado = "&id2=0&id3=0&id4=0&id5=0"
-        self.assertEqual(server.obter_relogio(), valor_esperado)
+        self.assertEqual(self.server.obter_relogio(), valor_esperado)
 
     @patch("requests.get")
     def teste_envia_com_sucesso(self, mock_get):
         mock_get.return_value = "teste_retorno"
-        server.relogio = {}
+        self.server.relogio = {}
         id_no = "1"
         acao = "w3"
         parametro_esperado = "http://172.38.0.1:5000?acao=w3&id2=0&id3=0&id4=0&id5=0"
         retorno_esperado = "this is ok: teste_retorno"
-        retorno = server.envia_acao(id_no, acao)
+        retorno = self.server.envia_acao(id_no, acao)
         mock_get.assert_called_with(parametro_esperado, timeout=1)
         self.assertEqual(retorno_esperado, retorno)
 
@@ -44,7 +47,7 @@ class TesteServer(unittest.TestCase):
         id_no = "1"
         acao = "w3"
         retorno_esperado = "not ok:"
-        retorno = server.envia_acao(id_no, acao)
+        retorno = self.server.envia_acao(id_no, acao)
         self.assertIn(retorno_esperado, retorno)
 
     def teste_recebe_acao(self):
