c4e1a64408c5438fcdf518e69677edc5d8a9619d
Rmaia237 <Rmaia237@github.com>
2017-10-12 14:25:07 -0300

    ajustes

--- server.py
+++ server.py
@@ -7,26 +7,37 @@ class Server(object):
     def __init__(self):
         self.ip = socket.gethostbyname(socket.gethostname())
         self.id = getenv("ID")
-        self.relogio = {}
+        self.num_servers = getenv("NUM_SERVERS")
+        self.vetor_relogios = self.__inicializa_vetor_relogios()
         self.url = "http://server{}:5000?"
 
-    def atualizar_relogio(self, query_string):
+    def __inicializa_vetor_relogios(self):
+        if self.num_servers:
+            vetor = {}
+            for i in range(int(self.num_servers)):
+                vetor["id{}".format(i)] = 0
+            return vetor
+        else:
+            return {"id1": 0, "id2": 0, "id3": 0, "id4": 0}
+
+    def atualizar_vetor_relogios(self, query_string):
         variaveis = str(query_string).split("&")
         for variavel in variaveis:
             if variavel.startswith("id"):
-                self.relogio[variavel.split("=")[0]] = variavel.split("=")[1]
+                id_no = variavel.split("=")[0]
+                valor = int(variavel.split("=")[1])
+                self.vetor_relogios[id_no] = valor
 
-    def obter_qs_relogio(self):
+    def obter_querystring_vetor_relogios(self):
         qs = ""
-        if not self.relogio:
-            self.relogio = {"id1": 0, "id2": 0, "id3": 0, "id4": 0}
-        for chave, valor in sorted(self.relogio.items()):
-            qs += "&{}={}".format(chave, valor)
+        for relogio, valor in sorted(self.vetor_relogios.items()):
+            qs += "&{}={}".format(relogio, valor)
         return qs
 
     def envia_acao(self, id_no, acao):
+        self.incrementa_relogio_interno()
         # TODO melhorar mensagem de retorno
-        qs = self.obter_qs_relogio()
+        qs = self.obter_querystring_vetor_relogios()
         try:
             retorno = requests.get(self.url.format(id_no) + "acao=" + acao + qs, timeout=1)
             msg = "this is ok: {}".format(retorno)
@@ -36,9 +47,11 @@ class Server(object):
 
     # TODO def incrementa_relogio_interno():
     def incrementa_relogio_interno(self):
-
-        pass
+        for relogio, valor in self.vetor_relogios.items():
+            if int(str(relogio).replace("id", "")) == self.id:
+                self.vetor_relogios[relogio] += 1
 
     # TODO def recebe_acao():
     def recebe_acao(self):
+        self.incrementa_relogio_interno()
         pass
--- teste_server.py
+++ teste_server.py
@@ -7,43 +7,45 @@ from server import Server
 
 class TesteServer(unittest.TestCase):
     def setUp(self):
+        self.id_no = 1
         self.server = Server()
+        self.server.id = self.id_no
 
     def test_atualizar_relogio(self):
         qs = "acao=teste_acao&id1=2&id2=3&id3=4&id4=5"
-        self.server.atualizar_relogio(qs)
+        self.server.atualizar_vetor_relogios(qs)
         tipo_esperado = dict
-        tipo_obtido = type(self.server.relogio)
-        valor_esperado = {"id1": "2", "id2": "3", "id3": "4", "id4": "5"}
-        valor_obtido = self.server.relogio
+        tipo_obtido = type(self.server.vetor_relogios)
+        valor_esperado = {"id1": 2, "id2": 3, "id3": 4, "id4": 5}
+        valor_obtido = self.server.vetor_relogios
         self.assertEqual(tipo_obtido, tipo_esperado)
         self.assertEqual(valor_obtido, valor_esperado)
 
     def teste_obter_qs_relogio(self):
         qs = "acao=teste_msg&id1=2&id2=3&id3=4&id4=5"
-        self.server.atualizar_relogio(qs)
-        relogio_esperado = {"id1": "2", "id2": "3", "id3": "4", "id4": "5"}
-        relogio_obtido = self.server.relogio
+        self.server.atualizar_vetor_relogios(qs)
+        relogio_esperado = {"id1": 2, "id2": 3, "id3": 4, "id4": 5}
+        relogio_obtido = self.server.vetor_relogios
         valor_esperado = "&id1=2&id2=3&id3=4&id4=5"
-        valor_obtido = self.server.obter_qs_relogio()
+        valor_obtido = self.server.obter_querystring_vetor_relogios()
         self.assertEqual(relogio_obtido, relogio_esperado)
         self.assertEqual(valor_obtido, valor_esperado)
 
     def teste_obter_qs_relogio_vazio(self):
-        self.server.relogio = {}
+        relogio_esperado = {"id1": 0, "id2": 0, "id3": 0, "id4": 0}
+        relogio_obtido = self.server.vetor_relogios
         valor_esperado = "&id1=0&id2=0&id3=0&id4=0"
-        valor_obtido = self.server.obter_qs_relogio()
+        valor_obtido = self.server.obter_querystring_vetor_relogios()
+        self.assertEqual(relogio_obtido, relogio_esperado)
         self.assertEqual(valor_obtido, valor_esperado)
 
     @patch("requests.get")
     def teste_envia_com_sucesso(self, mock_get):
         mock_get.return_value = "teste_retorno"
-        self.server.relogio = {}
-        id_no = "1"
         acao = "w3"
-        parametro_esperado = "http://server1:5000?acao=w3&id1=0&id2=0&id3=0&id4=0"
+        parametro_esperado = "http://server1:5000?acao=w3&id1=1&id2=0&id3=0&id4=0"
         retorno_esperado = "this is ok: teste_retorno"
-        retorno_obtido = self.server.envia_acao(id_no, acao)
+        retorno_obtido = self.server.envia_acao(self.id_no, acao)
         mock_get.assert_called_with(parametro_esperado, timeout=1)
         self.assertEqual(retorno_obtido, retorno_esperado)
 
@@ -56,10 +58,9 @@ class TesteServer(unittest.TestCase):
         self.assertIn(retorno_esperado, retorno_obtido)
 
     def teste_incrementa_relogio_interno(self):
-        self.server.relogio = {}
         self.server.incrementa_relogio_interno()
-        valor_esperado = ""
-        valor_obtido = ""
+        valor_esperado = {"id1": 1, "id2": 0, "id3": 0, "id4": 0}
+        valor_obtido = self.server.vetor_relogios
         self.assertEqual(valor_obtido, valor_esperado)
 
     def teste_recebe_acao(self):
