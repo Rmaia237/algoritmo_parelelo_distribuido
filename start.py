a355c6c974449b0e8ab21164aaac5a4b1ffa4bd1
Rodrigo de Carvalho Maia <rodrigo.maia@infoglobo.com.br>
2017-10-19 17:53:14 -0200

    Resolvendo problemas de envio de mensagens entre servidores

--- start.py
+++ start.py
@@ -5,16 +5,30 @@ from servidor import Servidor
 
 
 def retorna_query_string(resposta):
-    print(resposta.split(" ")[1])
+    print(type(resposta))
+    print(resposta)
+    # print(resposta.split(" ")[1])
 
 
 def conectado(conexao):
     resposta = str(conexao.recv(1024))
     retorna_query_string(resposta)
     print(server.obter_valores())
-    # print("Resposta: {}\n".format(resposta))
-    conexao.send("Conexao estabelecida\n")
+    print("Resposta: {}\n".format(resposta))
+    conexao.send("Conexao estabelecida\n".encode())
     conexao.close()
+
+
+def envia_mensagem():
+    print("enviando mensagem para o 2")
+    socket_id = socket()
+    socket_id.connect(("server2", int("5000")))
+    msg = "{}.{}".format(server.id, server.relogio_interno)
+    print("msg: '{}'".format(msg))
+    retorno = socket_id.send(msg.encode())
+    print(retorno)
+    socket_id.close()
+    return 0
 
 
 ip = "0.0.0.0"
@@ -28,8 +42,15 @@ tcp.listen(3)
 print("Server ID: {}".format(server.id))
 print("Ouvindo em {}:{}\n".format(ip, porta))
 
+enviar = True
 while True:
+    print(enviar)
+    print(server.id)
+    if enviar and server.id == 1:
+        envia_mensagem()
+        enviar = False
     con, origem = tcp.accept()
     print("Conexao recebida por: {}:{}\n".format(origem[0], origem[1]))
     thread = Thread(target=conectado, args=(con,))
     thread.start()
+
