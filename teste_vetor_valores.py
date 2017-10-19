239b7a9c8cee54f909f7938e3d6db8c1af34a512
Rodrigo de Carvalho Maia <rodrigo.maia@infoglobo.com.br>
2017-10-19 19:10:56 -0200

    Adicionando método que atualiza o vetor com os dados de um novo

--- teste_vetor_valores.py
+++ teste_vetor_valores.py
@@ -11,6 +11,20 @@ class TesteVetorValores(TestCase):
         valor_obtido = vetor_valores.obter_vetor_valores(num_servidores, id_servidor)
         self.assertEqual(valor_esperado, valor_obtido)
 
+    def teste_atualiza_faltando_valor(self):
+        vetor_original = [1, 2, "-", 4]
+        novo_vetor = ["-", 3, 4, "-"]
+        valor_esperado = [1, 3, 4, 4]
+        valor_obtido = vetor_valores.atualiza(vetor_original, novo_vetor)
+        self.assertEqual(valor_esperado, valor_obtido)
+
+    def teste_atualiza_com_todos_os_valores(self):
+        vetor_original = [1, 2, "-", 4]
+        novo_vetor = [5, 3, 4, 2]
+        valor_esperado = [5, 3, 4, 2]
+        valor_obtido = vetor_valores.atualiza(vetor_original, novo_vetor)
+        self.assertEqual(valor_esperado, valor_obtido)
+
 
 if __name__ == '__main__':
     main()
--- vetor_valores.py
+++ vetor_valores.py
@@ -1,4 +1,11 @@
 def obter_vetor_valores(num_servidores, id_servidor):
     vetor = ["-"] * num_servidores
-    vetor[id_servidor-1] = 0
+    vetor[id_servidor - 1] = 0
     return vetor
+
+
+def atualiza(vetor_local, outro_vetor):
+    for i in range(len(vetor_local)):
+        if outro_vetor[i] != "-":
+            vetor_local[i] = outro_vetor[i]
+    return vetor_local
